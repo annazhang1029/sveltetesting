d1d6f9409153f5ffa4a413c05dd8f03be58dfbd0
annazhang1029 <anna.zhang@duke.edu>
2023-06-08 14:00:31 -0400

    added another card

--- src/Card.svelte
+++ src/Card.svelte
@@ -7,12 +7,19 @@
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium itaque neque assumenda!</p>
 </div>
 
+<div class="card">
+	<img src="https://images.unsplash.com/photo-1596815064285-45ed8a9c0463?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=732&q=80" alt="profile picture" />
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium itaque neque assumenda!</p>
+</div>
+
+
  <style>
     @import url('https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&display=swap');
     .card{
         box-shadow: 0px 6px 6px rgba(0,0,0,0.1);
         padding: 20px;
         background-color:azure;
+        margin-bottom: 20px;
     }
 
     .card img{
